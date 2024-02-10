1ae9f4d437167f06b7e6e49f33b9bcfb3f65308e
Logon Aluno <logonrmlocal@fiap.com.br>
2024-02-09 22:43:10 -0300

    Primeiro App no react native

---
+++ Projeto01/App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Button } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Teste</Text>
+      <Button
+        title="Press me"
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#0fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
